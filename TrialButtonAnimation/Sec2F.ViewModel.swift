ce171987cfd38614db9a6eb3a8106716fc7517ed
Yoshifumi Sato <sato_yos@nifty.com>
2024-08-24 16:31:22 +0900

    Make Sec2F changable

--- TrialButtonAnimation/Sec2F.ViewModel.swift
+++ TrialButtonAnimation/Sec2F.ViewModel.swift
@@ -6,21 +6,41 @@
 //
 
 import Foundation
+import Combine
 
 extension Sec2F {
     class ViewModel: ObservableObject {
         @Published private(set) var secText: String
         private let startTime: CGFloat
         private let interval: CGFloat
+        private var timer: CountDownTimer
+        private var cancellables = Set<AnyCancellable>()
         
         init(startTime: CGFloat, interval: CGFloat) {
             self.startTime = startTime
             self.interval = interval
             self.secText = Self.strOf(time: startTime)
+            self.timer = CountDownTimer(startTime: startTime, intarval: interval)
+            buildPipeline()
         }
         
         private static func strOf(time: CGFloat) -> String {
             String(format: "%.2F", time)
         }
+        
+        private func buildPipeline() {
+            timer.$remainTime
+                .dropFirst()
+                .map{Self.strOf(time: $0)}
+                .sink { [weak self] string in
+                    self?.secText = string
+                }
+                .store(in: &cancellables)
+        }
+        
+        func startTimer() {
+            timer.start()
+        }
+        
     }
 }
--- TrialButtonAnimationTests/Sec2FViewModelTests.swift
+++ TrialButtonAnimationTests/Sec2FViewModelTests.swift
@@ -7,8 +7,10 @@
 
 @testable import TrialButtonAnimation
 import XCTest
+import Combine
 
 final class Sec2FViewModelTests: XCTestCase {
+    private var cancellables = Set<AnyCancellable>()
     
     func testInitViewModel() {
         // given
@@ -22,5 +24,17 @@ final class Sec2FViewModelTests: XCTestCase {
         let viewModel = Sec2F.ViewModel(startTime: 2.00, interval: 0.02)
         // then
         XCTAssertEqual(viewModel.secText, "2.00")
+        // when
+        viewModel.startTimer()
+        // then
+        let expectation = XCTestExpectation(description: "secText changes correctly!")
+        viewModel.$secText
+            .dropFirst()
+            .filter{$0 == "1.98"}
+            .sink { _ in
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        wait(for: [expectation], timeout: 0.1)
     }
 }
