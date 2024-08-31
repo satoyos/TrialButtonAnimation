baa410c68f728a4ab4134f883eec4f480667c156
Yoshifumi Sato <sato_yos@nifty.com>
2024-08-31 11:43:52 +0900

    Remove needless stored property in SecTimeWithButton View

--- TrialButtonAnimation/SecTimerWithButton.swift
+++ TrialButtonAnimation/SecTimerWithButton.swift
@@ -8,11 +8,9 @@
 import SwiftUI
 
 struct SecTimerWithButton {
-    @State private var startTime: Double
     @ObservedObject private var viewModel: ViewModel
     
     init(startTime: Double) {
-        self.startTime = startTime
         self.viewModel = ViewModel(startTime: startTime)
     }
 }
@@ -23,10 +21,9 @@ extension SecTimerWithButton: View {
             Sec2F(digitSize: 80, viewModel: viewModel.timeViewModel)
             Slider(value: Binding(
                 get: {
-                    startTime
+                    viewModel.timeViewModel.startTime
                 },
                 set: { newValue in
-                    self.startTime = newValue
                     viewModel.updateStartTime(to: newValue)
                 }), in: 1.5 ... 3.0, step: 0.02)
             .padding(.horizontal)
