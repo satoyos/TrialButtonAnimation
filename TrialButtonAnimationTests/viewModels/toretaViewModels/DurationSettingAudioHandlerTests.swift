8841bdece58a48f7170d6d1373bd0ab4b05c2dec
Yoshifumi Sato <sato_yos@nifty.com>
2024-11-19 20:24:36 +0900

    Add test action when player2 finished playing on DurationSettingAudioHandler

--- TrialButtonAnimationTests/viewModels/toretaViewModels/DurationSettingAudioHandlerTests.swift
+++ TrialButtonAnimationTests/viewModels/toretaViewModels/DurationSettingAudioHandlerTests.swift
@@ -20,11 +20,14 @@ final class DurationSettingAudioHandlerTests: XCTestCase {
     func testAfterPlayFinishedGivenClosureExecuted() throws {
         // given
         let handler = DurationSettingAudioHandler()
-        let expectation = XCTestExpectation(description: "Given closure executed")
-        handler.player1FinishedAction = { expectation.fulfill() }
+        let expectation1 = XCTestExpectation(description: "Given closure executed")
+        let expectation2 = XCTestExpectation(description: "Another Given closure executed")
+        handler.player1FinishedAction = { expectation1.fulfill() }
+        handler.player2FinishedAction = { expectation2.fulfill() }
         // when
         handler.startPlayer1()
+        handler.startPlayer2()
         // then
-        wait(for: [expectation], timeout: 10.0)
+        wait(for: [expectation1, expectation2], timeout: 10.0)
     }
 }
