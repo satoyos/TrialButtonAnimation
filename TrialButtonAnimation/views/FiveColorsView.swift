6c540c3704ad0dba4ff520ee93829bb0a529273e
Yoshifumi Sato <sato_yos@nifty.com>
2025-04-20 13:22:23 +0900

    Introduced enum FillType

---
+++ TrialButtonAnimation/views/FiveColorsView.swift
@@ -0,0 +1,18 @@
+//
+//  FiveColorsView.swift
+//  TrialButtonAnimation
+//
+//  Created by Yoshifumi Sato on 2025/04/01.
+//
+
+import SwiftUI
+
+struct FiveColorsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FiveColorsView()
+}
