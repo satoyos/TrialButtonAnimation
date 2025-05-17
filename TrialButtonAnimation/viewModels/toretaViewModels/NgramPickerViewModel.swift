ea41f7b2986cbe83e7bbf198c1bc0c73207b7243
Yoshifumi Sato <sato_yos@nifty.com>
2025-05-17 17:29:07 +0900

    Assign viewModel for each FirstChar cases

--- TrialButtonAnimation/viewModels/toretaViewModels/NgramPickerViewModel.swift
+++ TrialButtonAnimation/viewModels/toretaViewModels/NgramPickerViewModel.swift
@@ -7,7 +7,7 @@
 
 import Combine
 
-final class NgramPickerViewModel: ObservableObject {
+final class NgramPickerViewModel: ObservableObject, FillTypeHandlable {
   
   final class Input: InputObject {
     let firtCharButtonTapped = PassthroughSubject<FirstChar, Never>()
@@ -32,8 +32,69 @@ final class NgramPickerViewModel: ObservableObject {
     
     output.state100 = state100
     
+    FirstChar.allCases.forEach { firstChar in
+      let fillType = Self.fillType(of: firstChar, for: state100)
+      firstChar.buttonViewModel.input.setFillType.send(fillType)
+    }
+    
     self.input = input
     self.binding = binding
     self.output = output
+  }
+}
+
+extension NgramPickerViewModel {
+  static func fillType(of firstChar: FirstChar, for state100: SelectedState100) -> FillType {
+    fillType(of: state100.allSelectedNumbers, in: firstChar.poemNumbers)
+  }
+}
+
+fileprivate let justOneButtonViewModel = NgramButtonViewModel(firstChar: .justOne)
+fileprivate let uButtonViewModel = NgramButtonViewModel(firstChar: .u)
+fileprivate let tsuButtonViewModel = NgramButtonViewModel(firstChar: .tsu)
+fileprivate let shiButtonViewModel = NgramButtonViewModel(firstChar: .shi)
+fileprivate let moButtonViewModel = NgramButtonViewModel(firstChar: .mo)
+fileprivate let yuButtonViewModel = NgramButtonViewModel(firstChar: .yu)
+fileprivate let iButtonViewModel = NgramButtonViewModel(firstChar: .i)
+fileprivate let chiButtonViewModel = NgramButtonViewModel(firstChar: .chi)
+fileprivate let hiButtonViewModel = NgramButtonViewModel(firstChar: .hi)
+fileprivate let kiButtonViewModel = NgramButtonViewModel(firstChar: .ki)
+fileprivate let haButtonViewModel = NgramButtonViewModel(firstChar: .ha)
+fileprivate let yaButtonViewModel = NgramButtonViewModel(firstChar: .ya)
+fileprivate let yoButtonViewModel = NgramButtonViewModel(firstChar: .yo)
+fileprivate let kaButtonViewModel = NgramButtonViewModel(firstChar: .ka)
+fileprivate let miButtonViewModel = NgramButtonViewModel(firstChar: .mi)
+fileprivate let taButtonViewModel = NgramButtonViewModel(firstChar: .ta)
+fileprivate let koButtonViewModel = NgramButtonViewModel(firstChar: .ko)
+fileprivate let oButtonViewModel = NgramButtonViewModel(firstChar: .o)
+fileprivate let waButtonViewModel = NgramButtonViewModel(firstChar: .wa)
+fileprivate let naButtonViewModel = NgramButtonViewModel(firstChar: .na)
+fileprivate let aButtonViewModel = NgramButtonViewModel(firstChar: .a)
+
+extension FirstChar {
+  var buttonViewModel: NgramButtonViewModel {
+    switch self {
+    case .justOne: justOneButtonViewModel
+    case .u:   uButtonViewModel
+    case .tsu: tsuButtonViewModel
+    case .shi: shiButtonViewModel
+    case .mo:  moButtonViewModel
+    case .yu:  yuButtonViewModel
+    case .i:   iButtonViewModel
+    case .chi: chiButtonViewModel
+    case .hi:  hiButtonViewModel
+    case .ki:  kiButtonViewModel
+    case .ha:  haButtonViewModel
+    case .ya:  yaButtonViewModel
+    case .yo:  yoButtonViewModel
+    case .ka:  kaButtonViewModel
+    case .mi:  miButtonViewModel
+    case .ta:  taButtonViewModel
+    case .ko:  koButtonViewModel
+    case .o:   oButtonViewModel
+    case .wa:  waButtonViewModel
+    case .na:  naButtonViewModel
+    case .a:   aButtonViewModel
+    }
   }
 }
