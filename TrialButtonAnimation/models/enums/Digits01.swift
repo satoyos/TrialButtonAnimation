c89db9f06efa274a120626094e471b430d8207d3
Yoshifumi Sato <sato_yos@nifty.com>
2025-06-21 17:56:05 +0900

    Just introduced enum Dgits01 and its unit test

---
+++ TrialButtonAnimation/models/enums/Digits01.swift
@@ -0,0 +1,33 @@
+//
+//  Digits01.swift
+//  TrialButtonAnimation
+//
+//  Created by Yoshifumi Sato on 2025/06/21.
+//
+
+enum Digits01: Int {
+  case one   = 1
+  case two   = 2
+  case three = 3
+  case four  = 4
+  case five  = 5
+  case six   = 6
+  case seven = 7
+  case eight = 8
+  case nine  = 9
+  case zero  = 0
+}
+
+extension Digits01: CaseIterable {}
+
+extension Digits01 {
+  var pormNumbers: [Int] {
+    switch self {
+    case .zero:
+      return Array(1...10).map {$0 * 10}
+    default:
+      return Array(0...9)
+        .map {$0 * 10 + self.rawValue}
+    }
+  }
+}
---
+++ TrialButtonAnimationTests/models/Digits01Tests.swift
@@ -0,0 +1,30 @@
+//
+//  Digits01Tests.swift
+//  TrialButtonAnimationTests
+//
+//  Created by Yoshifumi Sato on 2025/06/21.
+//
+
+@testable import TrialButtonAnimation
+import XCTest
+
+final class Digits01Tests: XCTestCase {
+  
+  func testPoemNumbers() throws {
+    // given
+    let numbers2 = Digits01.two.pormNumbers
+    // then
+    XCTAssertEqual(numbers2, [2, 12, 22, 32, 42, 52, 62, 72, 82, 92])
+    // given
+    let numbers0 = Digits01.zero.pormNumbers
+    // then
+    XCTAssertEqual(numbers0, [10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    // size of poemNumbers is 10 in all cases
+    Digits01.allCases.forEach { digit01 in
+      XCTAssertEqual(digit01.pormNumbers.count, 10)
+    }
+  }
+  
+  
+  
+}
