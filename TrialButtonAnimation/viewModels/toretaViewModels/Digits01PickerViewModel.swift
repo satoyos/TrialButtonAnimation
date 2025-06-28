b6dbe77a1448a5eaf44afb9debef0c245787581c
Yoshifumi Sato <sato_yos@nifty.com>
2025-06-28 14:34:18 +0900

    Set fill type of each Button on Digits01Picker View

--- TrialButtonAnimation/viewModels/toretaViewModels/Digits01PickerViewModel.swift
+++ TrialButtonAnimation/viewModels/toretaViewModels/Digits01PickerViewModel.swift
@@ -7,7 +7,7 @@
 
 import Combine
 
-final class Digits01PickerViewModel: ViewModelObject {
+final class Digits01PickerViewModel: ViewModelObject, FillTypeHandlable {
   final class Input: InputObject {
     let setFillType = PassthroughSubject<FillType, Never>()
   }
@@ -31,8 +31,55 @@ final class Digits01PickerViewModel: ViewModelObject {
     
     output.state100 = state100
     
+    Digits01.allCases.forEach { digit01 in
+      let fillType = Self.fillType(of: digit01, for: state100)
+      digit01.buttonViewModel.input.setFillType.send(fillType)
+    }
+    
     self.input = input
     self.binding = binding
     self.output = output
+  }
+}
+
+extension Digits01PickerViewModel {
+  var selectedNum: Int {
+    output.state100.selectedNum
+  }
+}
+
+extension Digits01PickerViewModel {
+  static func fillType(of digts01: Digits01,
+                       for state100: SelectedState100) -> FillType {
+    fillType(of: state100.allSelectedNumbers,
+             in: digts01.pormNumbers)
+  }
+}
+
+fileprivate let oneButtonViewModel = Digits01ButtonViewModel(digit01: .one)
+fileprivate let twoButtonViewModel = Digits01ButtonViewModel(digit01: .two)
+fileprivate let threeButtonViewModel = Digits01ButtonViewModel(digit01: .three)
+fileprivate let foourButtonViewModel = Digits01ButtonViewModel(digit01: .four)
+fileprivate let fiveButtonViewModel = Digits01ButtonViewModel(digit01: .five)
+fileprivate let sixButtonViewModel = Digits01ButtonViewModel(digit01: .six)
+fileprivate let sevenButtonViewModel = Digits01ButtonViewModel(digit01: .seven)
+fileprivate let eightButtonViewModel = Digits01ButtonViewModel(digit01: .eight)
+fileprivate let nineButtonViewModel = Digits01ButtonViewModel(digit01: .nine)
+fileprivate let zeroButtonViewModel = Digits01ButtonViewModel(digit01: .zero)
+
+extension Digits01 {
+  var buttonViewModel: Digits01ButtonViewModel {
+    switch self {
+    case .one:   oneButtonViewModel
+    case .two:   twoButtonViewModel
+    case .three: threeButtonViewModel
+    case .four:  foourButtonViewModel
+    case .five:  fiveButtonViewModel
+    case .six:   sixButtonViewModel
+    case .seven: sevenButtonViewModel
+    case .eight: eightButtonViewModel
+    case .nine:  nineButtonViewModel
+    case .zero:  zeroButtonViewModel
+    }
   }
 }
--- TrialButtonAnimationTests/viewModels/toretaViewModels/Digits01PickerViewModelTests.swift
+++ TrialButtonAnimationTests/viewModels/toretaViewModels/Digits01PickerViewModelTests.swift
@@ -18,4 +18,14 @@ final class Digits01PickerViewModelTests: XCTestCase {
     XCTAssertNotNil(viewModel)
   }
   
+  func testPartialSelected() {
+      // given
+      let state100 = SelectedState100()
+        .cancelOf(number: 13) // 「つくばねの」を選択から外す
+      // when
+      let viewModel = Digits01PickerViewModel(state100: state100)
+      // then
+      XCTAssertEqual(viewModel.selectedNum, 99)
+    XCTAssertEqual(Digits01.three.buttonViewModel.output.fillType, .partial)
+    }
 }
