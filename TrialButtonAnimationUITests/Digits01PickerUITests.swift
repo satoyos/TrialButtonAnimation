37a74848acd819f59c6e6bcbfbd36852d8f94270
Yoshifumi Sato <sato_yos@nifty.com>
2025-07-05 15:51:11 +0900

    Add 2 UItests for Digits01Picker View

--- TrialButtonAnimationUITests/Digits01PickerUITests.swift
+++ TrialButtonAnimationUITests/Digits01PickerUITests.swift
@@ -39,7 +39,40 @@ final class Digits01PickerUITests: XCTestCase {
     // then
     let theButton = homePage.digitPicker01Button
     XCTAssert(theButton.staticTexts["90首"].exists)
-    
   }
 
+  func testTapEmptyChangesItToFull() {
+    // given
+    let digitsPage = homePage.gotoDigitPickerPage01()
+    XCTAssert(digitsPage.badge(of: 100).exists)
+    digitsPage.tapCell(number: 2)
+    XCTAssert(digitsPage.badge(of: 90).exists)
+    // when
+    digitsPage.tapCell(number: 2)
+    // then
+    XCTAssert(digitsPage.badge(of: 100).exists)
+    // when
+    digitsPage.backToTopButton.tap()
+    // then
+    let theButton = homePage.digitPicker01Button
+    XCTAssert(theButton.staticTexts["100首"].exists)
+  }
+  
+  func testTapSeveralCells() {
+    // given
+    let digitsPage = homePage.gotoDigitPickerPage01()
+    XCTAssert(digitsPage.badge(of: 100).exists)
+    // when
+    digitsPage
+      .tapCell(number: 1)
+      .tapCell(number: 3)
+      .tapCell(number: 4)
+      .tapCell(number: 7)
+    // then
+    XCTAssert(digitsPage.badge(of: 60).exists)
+    // when
+    digitsPage.backToTopButton.tap()
+    let theButton = homePage.digitPicker01Button
+    XCTAssert(theButton.staticTexts["60首"].exists)
+  }
 }
