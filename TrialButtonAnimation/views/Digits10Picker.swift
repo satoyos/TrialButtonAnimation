20381924ed76cf05ed41daaaab3d18ddbc05eed7
Yoshifumi Sato <sato_yos@nifty.com>
2025-07-06 18:10:43 +0900

    Introduced Digits10Picer View

---
+++ TrialButtonAnimation/views/Digits10Picker.swift
@@ -0,0 +1,53 @@
+//
+//  Digits10Picker.swift
+//  TrialButtonAnimation
+//
+//  Created by Yoshifumi Sato on 2025/07/06.
+//
+
+import SwiftUI
+
+struct Digits10Picker {
+  let settings: Settings
+  @ObservedObject private var viewModel: Digits10PickerViewModel
+  
+  init(settings: Settings) {
+    self.settings = settings
+    self.viewModel = .init(state100: settings.state100)
+  }
+}
+
+extension Digits10Picker: View {
+  var body: some View {
+    NavigationStack {
+      List {
+        ForEach(Digits10.allCases) { digit in
+          Digits10Button(viewModel: digit.buttonViewModel) {
+            viewModel.input.digitButtonTapped.send(digit)
+          }
+          .accessibilityIdentifier(digit.description)
+        }
+      }
+      .toolbar {
+        ToolbarItem(placement: .confirmationAction) {
+          BadgeView(number: viewModel.selectedNum)
+        }
+        ToolbarItem(placement: .principal) {
+          Text("10の位の数で選ぶ")
+        }
+      }
+    }
+//    .onChange(of: isPresented) {
+//      guard !isPresented else { return }
+//      tasksForLeavingThisView()
+//    }
+  }
+  
+  func tasksForLeavingThisView() {
+      settings.state100 = viewModel.output.state100
+  }
+}
+
+#Preview {
+  Digits10Picker(settings: Settings())
+}
