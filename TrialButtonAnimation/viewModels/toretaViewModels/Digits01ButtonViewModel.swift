3a2a3b1b40b30aa3310fbb46972a51deedef776d
Yoshifumi Sato <sato_yos@nifty.com>
2025-07-13 11:10:26 +0900

    Refactor: Simplify Digits01ButtonViewModel and Digits10ButtonVIewModel by uisng common base class DigitsButtonViewModel

--- TrialButtonAnimation/viewModels/toretaViewModels/Digits01ButtonViewModel.swift
+++ TrialButtonAnimation/viewModels/toretaViewModels/Digits01ButtonViewModel.swift
@@ -5,50 +5,52 @@
 //  Created by Yoshifumi Sato on 2025/06/21.
 //
 
-import Combine
+//import Combine
 
-final class Digits01ButtonViewModel: ViewModelObject {
-  final class Input: InputObject {
-    let setFillType = PassthroughSubject<FillType, Never>()
-  }
-  
-  final class Binding: BindingObject {
-  }
-  
-  final class Output: OutputObject {
-    @Published var fillType: FillType = .full
-  }
-  
-  let input: Input
-  @BindableObject private(set) var binding: Binding
-  let output: Output
-  
-  let digit: Digits01
-  private var cancellables: Set<AnyCancellable> = []
-  
-  init(digit: Digits01) {
-    let input = Input()
-    let binding = Binding()
-    let output = Output()
-    
-    output.fillType = .full
-    
-    input.setFillType
-      .assign(to: \.fillType, on: output)
-      .store(in: &cancellables)
-    
-    self.digit = digit
-    self.input = input
-    self.binding = binding
-    self.output = output
-  }
-}
+final class Digits01ButtonViewModel: DigitsButtonViewModel<Digits01> {}
 
-extension Digits01ButtonViewModel {
-  var numbersDescription: String {
-    "歌番号: " +
-    digit.poemNumbers.description
-      .dropFirst()
-      .dropLast()
-  }
-}
+//final class Digits01ButtonViewModel: ViewModelObject {
+//  final class Input: InputObject {
+//    let setFillType = PassthroughSubject<FillType, Never>()
+//  }
+//  
+//  final class Binding: BindingObject {
+//  }
+//  
+//  final class Output: OutputObject {
+//    @Published var fillType: FillType = .full
+//  }
+//  
+//  let input: Input
+//  @BindableObject private(set) var binding: Binding
+//  let output: Output
+//  
+//  let digit: Digits01
+//  private var cancellables: Set<AnyCancellable> = []
+//  
+//  init(digit: Digits01) {
+//    let input = Input()
+//    let binding = Binding()
+//    let output = Output()
+//    
+//    output.fillType = .full
+//    
+//    input.setFillType
+//      .assign(to: \.fillType, on: output)
+//      .store(in: &cancellables)
+//    
+//    self.digit = digit
+//    self.input = input
+//    self.binding = binding
+//    self.output = output
+//  }
+//}
+//
+//extension Digits01ButtonViewModel {
+//  var numbersDescription: String {
+//    "歌番号: " +
+//    digit.poemNumbers.description
+//      .dropFirst()
+//      .dropLast()
+//  }
+//}
